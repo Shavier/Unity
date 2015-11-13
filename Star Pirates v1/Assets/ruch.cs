4f5c5fcdab48ddb4a02e5d4cfc899ea8059e3ae5
Adrian <adino1191@gmail.com>
2015-11-14 00:01:44 +0100

    Pierwsza Wersja

---
+++ Star Pirates v1/Assets/ruch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ruch : MonoBehaviour {
+
+    public float moveSpeed = 2f;
+    public Vector2 speed = new Vector2(10, 10);
+    Rigidbody2D rigid;
+    private Vector2 movement;
+
+
+
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+	
+
+	void Update () {
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY
+            );
+
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = movement;
+    }
+}
